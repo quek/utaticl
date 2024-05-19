4470bbc4fbbb890c65d27be0dcb441b29fe0ee94
Tahara Yoshinori <read.eval.print@gmail.com>
2024-05-19 14:35:27 +0900

    どこまでも横スクロール

--- src/time-ruler-mixin.lisp
+++ src/time-ruler-mixin.lisp
@@ -1,9 +1,5 @@
 (in-package :dgw)
 
-(defmethod max-bar ((self time-ruler-mixin))
-  ;; TODO
-  8)
-
 (defmethod render-playhead ((self time-ruler-mixin))
   (let* ((draw-list (ig:get-window-draw-list))
          (window-pos (ig:get-window-pos))
@@ -14,21 +10,24 @@
 
 (defmethod render-time-ruler ((self time-ruler-mixin))
   (let* ((draw-list (ig:get-window-draw-list))
-         (max-bar (max-bar self))
          (window-pos (ig:get-window-pos))
          (window-size (ig:get-window-size))
          (scroll-x (ig:get-scroll-x))
          (scroll-y (ig:get-scroll-y)))
-    (loop for bar from 0 to max-bar
+    (loop for bar from 0
           for x = (+ (* bar 4 (.zoom-x self))
                      (.offset-x self))
           for cursor-pos = (list x scroll-y)
-          do (when (<= (+ (.offset-x self) scroll-x) (.x cursor-pos))
-               (ig:set-cursor-pos cursor-pos)
+          if (<= (+ scroll-x (.x window-size)) (.x cursor-pos))
+            do (ig:set-cursor-pos (@+ cursor-pos (@ 100.0 .0)))
+               (ig:text " ")
+               (loop-finish)
+          if (<= (+ (.offset-x self) scroll-x) (.x cursor-pos))
+            do (ig:set-cursor-pos cursor-pos)
                (ig:text (format nil " ~d" (1+ bar)))
                (let* ((p1 (@+ cursor-pos window-pos (@ (- scroll-x) (- scroll-y))))
                       (p2 (@+ p1 (@ 0.0 (.y window-size)))))
                  (when (<= (+ (.offset-x self) (.x window-pos)) (.x p1))
-                   (ig:add-line draw-list p1 p2 (.color-line *theme*))))))
+                   (ig:add-line draw-list p1 p2 (.color-line *theme*)))))
 
     (render-playhead self)))
