5138fc2b7217e789f06812662f3681b87ebc821b
Tahara Yoshinori <read.eval.print@gmail.com>
2024-09-28 16:17:47 +0900

    clap

--- src/module.lisp
+++ src/module.lisp
@@ -21,8 +21,6 @@
 
 (sb-ext:defglobal *hwnd-module-map* (make-hash-table :weakness :value))
 
-(defmethod bus-changed ((self module) nbuses))
-
 (defmethod connect ((from module) (to module))
   (let ((connection (make-instance 'connection
                                         :from from
