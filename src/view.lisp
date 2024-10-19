9f685b6b199393113d7b6cd3f5dfe3e0dbc249e0
Tahara Yoshinori <read.eval.print@gmail.com>
2024-10-19 13:08:15 +0900

    automation 編集の最低限の機能実装

--- src/view.lisp
+++ src/view.lisp
@@ -8,11 +8,15 @@
   (@@ (@+ *window-pos* (@ (.offset-x self) (.offset-y self)))
       (@+ *window-pos* *window-size* (@ 0.0 (- *scrollbar-size*)))))
 
-(defmethod world-y-to-time ((self view) y)
-  (/ (coerce (+ (- y (.y (ig:get-window-pos)) (.offset-y self))
-                (ig:get-scroll-y))
-             'double-float)
-     (coerce (.zoom-y self) 'double-float)))
+(defmethod world-y-to-time ((self view) y &optional grid-snap)
+  (let ((time
+          (/ (coerce (+ (- y (.y (ig:get-window-pos)) (.offset-y self))
+                        (ig:get-scroll-y))
+                     'double-float)
+             (coerce (.zoom-y self) 'double-float))))
+    (if grid-snap
+        (time-grid-applied self time grid-snap)
+        time)))
 
 
 
