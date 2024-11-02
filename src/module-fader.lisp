05cce6bdfc5db5c96dec34a19cbedb0bcb27d49a
Tahara Yoshinori <read.eval.print@gmail.com>
2024-11-02 11:54:28 +0900

    pan

--- src/module-fader.lisp
+++ src/module-fader.lisp
@@ -7,16 +7,31 @@
   (param-add self (make-instance 'param :id 'volume :name "Volume" :value 1.0d0)))
 
 (defmethod render ((module-fader module-fader))
-  (ig:drag-scalar "VOL"
+  (ig:drag-scalar "Vol"
                   ig:+im-gui-data-type-double+
                   (.value (param module-fader 'volume))
                   :speed .01
                   :min .0d0
                   :max 1.0d0
+                  :format "%.2f")
+  (ig:drag-scalar "Pan"
+                  ig:+im-gui-data-type-double+
+                  (.value (param module-fader 'pan))
+                  :speed .01
+                  :min .0d0
+                  :max 1.0d0
                   :format "%.2f"))
 
-(defmethod process-sample ((self module-fader) sample)
+(defmethod process-sample ((self module-fader) sample0 sample1)
   ;; TODO
-  (coerce (* sample (.value (param self 'volume)))
-          'single-float))
+  (values (coerce (* sample0
+                     (.value (param self 'volume))
+                     (* (- 1.0 (.value (param self 'pan)))
+                        2.0))
+                  'single-float)
+          (coerce (* sample1
+                     (.value (param self 'volume))
+                     (* (.value (param self 'pan))
+                        2.0))
+                  'single-float)))
 
--- src/module-gain.lisp
+++ src/module-gain.lisp
@@ -3,7 +3,9 @@
 (defmethod initialize-instance :after ((self module-gain) &key)
   (param-add self (make-instance 'param :id 'volume :name "Volume" :value 1.0d0)))
 
-(defmethod process-sample ((self module-gain) sample)
-  (coerce (* sample (.value (param self 'volume)))
-          'single-float))
+(defmethod process-sample ((self module-gain) sample0 sample1)
+  (values (coerce (* sample0 (.value (param self 'volume)))
+                  'single-float)
+          (coerce (* sample1 (.value (param self 'volume)))
+                  'single-float)))
 
