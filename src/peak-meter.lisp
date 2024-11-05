1fb510193d146798517d16f90e43329113e054d2
Tahara Yoshinori <read.eval.print@gmail.com>
2024-11-05 12:15:33 +0900

    peak-meter

--- src/peak-meter.lisp
+++ src/peak-meter.lisp
@@ -4,17 +4,18 @@
   (loop for value in values
         for i from 0
         for peak-value = (nth i (.values self))
-        for peak-at = (nth i (.ats self))
         if (< peak-value value)
           do (setf (nth i (.values self)) value)
-             (setf (nth i (.ats self)) (get-internal-real-time))
-        if (< (* internal-time-units-per-second 2)
-              (- (get-internal-real-time) peak-at))
-          do (setf (nth i (.values self)) (max 0.0 (- peak-value 0.01)))))
+             (setf (nth i (.ats self)) (get-internal-real-time))))
 
 (defmethod render ((self peak-meter))
   (loop for value in (.values self)
         for at in (.ats self)
-        do (ig:text (format nil "~,3f ~a"
+        for i from 0
+        do (ig:slider-float "L" value 0.0 1.0)
+           (ig:text (format nil "~,3f ~a"
                             value
-                            at))))
+                            at))
+        if (< (* internal-time-units-per-second 1)
+              (- (get-internal-real-time) at))
+          do (setf (nth i (.values self)) (max 0.0 (- value 0.003)))))
