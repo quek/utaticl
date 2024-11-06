e45671a6bfe94336d9df8a1be5d5d4560907d255
Tahara Yoshinori <read.eval.print@gmail.com>
2024-11-06 13:00:11 +0900

    dummy

--- src/peak-meter.lisp
+++ src/peak-meter.lisp
@@ -22,16 +22,16 @@
         do (setf (nth i (.avgs self))
                  (if (< avg avg-tmp)
                      avg-tmp
-                     (+ (* avg 0.98)
-                        (* avg-tmp 0.02))))))
+                     (+ (* avg 0.9)
+                        (* avg-tmp 0.2))))))
 
 (defmethod render-in ((self peak-meter) (rack rack) &key)
   (let* ((size (@ 21.0 (- (.y *window-size*) *scrollbar-size*
-                          (plus-c:c-ref *style* ig:im-gui-style :item-spacing :y))))
+                          (* 2 (plus-c:c-ref *style* ig:im-gui-style :item-spacing :y)))))
          (cursor-pos (ig:get-cursor-pos))
          (pos1 (local-to-world rack cursor-pos))
          (pos2 (@+ pos1 size)))
-    (ig:invisible-button "##" size)
+    (ig:dummy size)
     (ig:add-rect-filled *draw-list* pos1 pos2 (color #x03 #x03 #x03 #x80))
     (loop for value in (.values self)
           for at in (.ats self)
@@ -47,27 +47,24 @@
                                     (* (.y size) (- 1.0 peak-normalized)))))
                     (p2 (@+ p1 (@ (/ (.x size) 2) 3.0))))
                (ig:add-rect-filled *draw-list* p1 p2 (color #xff #xff #x00 #xcc)))
-
           if (< (* internal-time-units-per-second 1)
                 (- (get-internal-real-time) at))
             do (setf (nth i (.values self))
                      ;; こっちはリニアに下がった方がよさそう
-                     (max 0.0 (- value 0.003)))))
-
-  (ig:same-line)
-  (ig:with-group
-    (loop for value in (.values self)
-          for at in (.ats self)
-          for label in '("L" "R")
-          for avg in (.avgs self)
-          for i from 0
-          for peak-db = (to-db-float value)
-          for avg-db = (to-db-float avg)
-          do (ig:text (format nil "~a ~,2fdb ~,2fdb" label peak-db avg-db)))))
+                     (max 0.0 (- value 0.003))))
+    (loop for db in '(6 0 -6 -12 -18 -32 -64)
+          for pos = (@+ cursor-pos
+                        (@ 0.0
+                           (* (.y size)
+                              (- 1.0 (%peak-meter-db-to-normalized db)))))
+          do (ig:set-cursor-pos pos)
+             (ig:text (format nil "~d" db)))))
 
 (defun %peak-meter-db-to-normalized (db)
-  "最大が 6db 最小が -90db"
-  (let ((normalized (/ (+ db 90.0) 96.0)))
+  "最大が 6db 最小が -180db"
+  (let ((normalized (expt (/ (- db -180.0)
+                             (- 6.0 -180.0))
+                          7.0)))
     (if (< normalized 0.0)
         0.0
         (if (> normalized 1.0)
